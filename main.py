7b1b558eef245492f6fcd00409931b21eb9cb814
Aman Gautam <129865249+amangautamdev@users.noreply.github.com>
2024-04-14 20:39:18 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+# amangautamdev
+# Don't Remove Credit 🥺
+# Telegram Channel @LegendUnion
+# Backup Channel @LegendDawnload
+# Developer @LegendRobot
+
+
+from bot import Bot
+
+app = Bot()
+app.run()
+
+
+
+
+
+
+# amangautamdev
+# Don't Remove Credit 🥺
+# Telegram Channel @LegendUnion
+# Backup Channel @LegendDawnload
+# Developer @LegendRobot
